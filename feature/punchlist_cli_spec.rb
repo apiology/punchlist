bd195459ed7bbde6f84f4422125fcef600c30711
Vince Broz <vince@broz.cc>
2015-04-07 19:20:45 -0400

    Add non_source_file_with_pis test case

--- feature/punchlist_cli_spec.rb
+++ feature/punchlist_cli_spec.rb
@@ -6,10 +6,11 @@ describe 'bigfiles' do
       .to eq("USAGE: punchlist\n")
   end
 
-  # "pis" are "punchlist items" non_source_file mixed_set
+  # "pis" are "punchlist items" non_source_file_with_pis mixed_set
   # more_than_once_source_file three_files four_files
   # swift_and_ruby_files swift_zorb_and_ruby_files
-  %w(no_files source_file_with_no_items).each do |type|
+  %w(no_files source_file_with_no_items
+     non_source_file_with_pis).each do |type|
     it "handles #{type} case" do
       expect(exec_io "cd feature/samples/#{type} &&" \
                      'RUBYLIB=`pwd`/../../lib:"$RUBYLIB" punchlist')
