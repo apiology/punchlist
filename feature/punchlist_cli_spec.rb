a50f36d6ff567c8ca82852aeda75ceb28501d23d
Vince Broz <vince@broz.cc>
2015-04-07 19:11:20 -0400

    Get no-file case to work

--- feature/punchlist_cli_spec.rb
+++ feature/punchlist_cli_spec.rb
@@ -6,14 +6,12 @@ describe 'bigfiles' do
       .to eq("USAGE: punchlist\n")
   end
 
-  # three_files one_file two_files some_nonsource_files many_files
-  # zero_byte_file
-  %w(no_files three_files four_files swift_and_ruby_files
-     swift_zorb_and_ruby_files).each do |type|
-    xit "handles #{type} case" do
+  # no_files three_files four_files swift_and_ruby_files
+  # swift_zorb_and_ruby_files
+  %w(no_files).each do |type|
+    it "handles #{type} case" do
       expect(exec_io "cd feature/samples/#{type} &&" \
-                     'RUBYLIB=`pwd`/../../lib:"$RUBYLIB" bigfiles ' \
-                     "--glob '*.{rb,swift,zorb}'")
+                     'RUBYLIB=`pwd`/../../lib:"$RUBYLIB" punchlist')
         .to eq(IO.read("feature/expected/#{type}_results.txt"))
     end
   end
