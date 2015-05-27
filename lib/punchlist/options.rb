2713a659b1046c6f4b8c76f13cd13c08e3b9b046
Vince Broz <vince@broz.cc>
2015-05-27 13:00:07 -0400

    Add Options class for option parsing

---
+++ lib/punchlist/options.rb
@@ -0,0 +1,33 @@
+require 'optparse'
+
+module Punchlist
+  class Options
+    attr_reader :default_punchlist_line_regexp
+
+    def initialize(default_punchlist_line_regexp)
+      @default_punchlist_line_regexp = default_punchlist_line_regexp
+    end
+
+    def setup_options(opts)
+      options = {}
+      opts.banner = 'Usage: punchlist [options]'
+      opts.on('-g', '--glob g', 'Filename glob to identify source files') do |v|
+        options[:glob] = v
+      end
+      opts.on('-r', '--regexp r',
+              'Regexp to trigger on - ' \
+              'default is XXX|TODO') do |v|
+        options[:regexp] = v
+      end
+      options
+    end
+
+    def parse_options
+      options = nil
+      OptionParser.new do |opts|
+        options = setup_options(opts)
+      end.parse!
+      options
+    end
+  end
+end
