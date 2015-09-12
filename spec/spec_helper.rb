59f2948d36a2dee0910c0387aef570bd1c464c47
Vince Broz <vince@broz.cc>
2015-09-12 16:08:06 -0400

    Avoid rspec deprecation and just use standard feature instead

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,6 @@ SimpleCov.refuse_coverage_drop
 require 'punchlist'
 
 RSpec.configure do |config|
-  config.filter_run_excluding :wip
   config.run_all_when_everything_filtered = true
   config.order = 'random'
   config.alias_it_should_behave_like_to :has_behavior
