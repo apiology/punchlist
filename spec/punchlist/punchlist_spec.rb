ff770def3ad66d59befa45ae7f1f2a1961ac1ad5
Vince Broz <vince.broz@bluelabs.com>
2019-12-21 14:17:58 -0500

    RSpec cleanup: subject(:whatever) -> let(:whatever)

--- spec/punchlist/punchlist_spec.rb
+++ spec/punchlist/punchlist_spec.rb
@@ -5,23 +5,24 @@ require 'punchlist'
 
 describe Punchlist::Punchlist do
   let_double :outputter, :globber, :file_opener, :exiter, :source_file_globber
-  subject(:punchlist) do
+  let(:punchlist) do
     described_class.new(args,
                         outputter: outputter,
                         file_opener: file_opener,
                         source_file_globber: source_file_globber)
   end
-  subject(:args) { [] }
+  let(:args) { [] }
 
   context 'with real arguments' do
-    subject(:files_found) { file_contents.keys }
-    subject(:expected_glob) do
+    let(:files_found) { file_contents.keys }
+    let(:expected_glob) do
       '{app,lib,test,spec,feature}/**/' \
       '*.{rb,swift,scala,js,cpp,c,java,py}'
     end
-    subject(:expected_exclude_glob) do
+    let(:expected_exclude_glob) do
       nil
     end
+
     def expect_globs_assigned
       allow(source_file_globber).to(receive(:source_files_glob=))
                                 .with(expected_glob)
@@ -40,12 +41,14 @@ describe Punchlist::Punchlist do
     end
 
     context 'with no arguments' do
-      subject(:args) { [] }
+      let(:args) { [] }
+
       context 'and no files found' do
-        subject(:file_contents) do
+        let(:file_contents) do
           {}
         end
-        subject(:expected_output) { '' }
+        let(:expected_output) { '' }
+
         it 'runs' do
           punchlist.run
         end
@@ -53,46 +56,49 @@ describe Punchlist::Punchlist do
 
       context 'and we found' do
         context 'a ruby file' do
-          subject(:expected_output) do
+          let(:expected_output) do
             "foo.rb:3: puts 'foo' # XXX change to bar\n"
           end
-          subject(:file_contents) do
+          let(:file_contents) do
             {
               'foo.rb' => "#\n#\n" \
                           "puts 'foo' # XXX change to bar\n",
             }
           end
+
           it 'runs' do
             punchlist.run
           end
         end
 
         context 'a scala file' do
-          subject(:expected_output) do
+          let(:expected_output) do
             "bar.scala:5: println('zing') # XXX change to foo\n"
           end
-          subject(:file_contents) do
+          let(:file_contents) do
             {
               'bar.scala' => "#\n#\n#\n#\n" \
                              "println('zing') # XXX change to foo\n",
             }
           end
+
           it 'runs' do
             punchlist.run
           end
         end
 
         context 'a scala file with no entries' do
-          subject(:expected_output) do
+          let(:expected_output) do
             ''
           end
-          subject(:file_contents) do
+          let(:file_contents) do
             {
               'bar.scala' => "#\n#\n#\n#\n" \
                              "println('zing') #  change to foo\n",
               'foo.rb' => '',
             }
           end
+
           it 'runs' do
             punchlist.run
           end
@@ -103,19 +109,19 @@ describe Punchlist::Punchlist do
     context 'with glob argument excluding scala' do
       context 'and we found' do
         context 'a ruby and scala file' do
-          subject(:expected_output) do
+          let(:expected_output) do
             "foo.rb:3: puts 'foo' # XXX change to bar\n"
           end
-          subject(:expected_glob) do
+          let(:expected_glob) do
             '**/*.rb'
           end
-          subject(:file_contents) do
+          let(:file_contents) do
             {
               'foo.rb' => "#\n#\n" \
                           "puts 'foo' # XXX change to bar\n",
             }
           end
-          subject(:args) { ['--glob', '**/*.rb'] }
+          let(:args) { ['--glob', '**/*.rb'] }
 
           it 'runs' do
             punchlist.run
@@ -127,11 +133,11 @@ describe Punchlist::Punchlist do
     context 'with regexp argument adding something' do
       context 'and we found' do
         context 'a ruby and scala file' do
-          subject(:expected_output) do
+          let(:expected_output) do
             "foo.rb:3: puts 'foo' # FUTURE change to bar\n"
           end
-          subject(:expected_glob) { '**/*.rb' }
-          subject(:file_contents) do
+          let(:expected_glob) { '**/*.rb' }
+          let(:file_contents) do
             {
               'foo.rb' => "#\n#\n" \
                           "puts 'foo' # FUTURE change to bar\n",
@@ -139,15 +145,17 @@ describe Punchlist::Punchlist do
           end
 
           context 'and no exclusions' do
-            subject(:args) { ['--glob', '**/*.rb', '--regexp', 'FUTURE'] }
+            let(:args) { ['--glob', '**/*.rb', '--regexp', 'FUTURE'] }
+
             it('runs') { punchlist.run }
           end
           context 'with exclusions' do
-            subject(:args) do
+            let(:args) do
               ['--glob', '**/*.rb', '--regexp', 'FUTURE',
                '--exclude-glob', 'lib/foo/baz.rb']
             end
-            subject(:expected_exclude_glob) { 'lib/foo/baz.rb' }
+            let(:expected_exclude_glob) { 'lib/foo/baz.rb' }
+
             it('runs') { punchlist.run }
           end
         end
