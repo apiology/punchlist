459d8e870c68ea30a953924f4803a86fed5bf18b
Vince Broz <vince@broz.cc>
2024-10-06 10:34:11 -0400

    Better handle binary files accidentally passed in (#17)
    * Silently handle binary files accidentally included
    
    * Fix coverage

--- spec/punchlist/inspector_spec.rb
+++ spec/punchlist/inspector_spec.rb
@@ -20,9 +20,11 @@ describe Punchlist::Inspector do
   describe '#run' do
     subject(:inspector_run_results) { inspector.run }
 
+    let(:contents_io) { StringIO.new(contents) }
+
     before do
       allow(file_opener).to(receive(:open)).with(filename, 'r')
-                        .and_yield(StringIO.new(contents))
+        .and_yield(contents_io)
     end
 
     context 'with no lines in source file' do
@@ -41,6 +43,28 @@ describe Punchlist::Inspector do
       let(:contents) { 'MYPL: Add some code here' }
 
       it { is_expected.to eq [Punchlist::Offense.new(filename, 1, contents)] }
+    end
+
+    context 'with a binary file passed' do
+      # contents with non-UTF8 binary data
+      let(:contents) { "MYPL: Add some code here\x80" }
+
+      it { is_expected.to eq [] }
+    end
+
+    context 'with some other error' do
+      let(:contents_io) { instance_double(StringIO, 'contents_io') }
+      # create a mock
+      let(:contents) { instance_double(String, 'contents') }
+
+      before do
+        allow(contents_io).to receive(:each_line).and_yield(contents)
+        allow(contents).to receive(:=~).and_raise(ArgumentError.new('foo'))
+      end
+
+      it 'propogates the error' do
+        expect { inspector_run_results }.to raise_error(ArgumentError)
+      end
     end
 
     context 'with two relevant lines in source file' do
